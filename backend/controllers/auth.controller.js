cf12a76b7a7de0901534b2afee8a5c53a2cad2e7
Shanidhya01 <luckykumar0011s@gmail.com>
2026-01-18 16:37:53 +0530

    fixed login issyue after deplo

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,11 +1,11 @@
-const { setToken } = require("../utils/jwt");
+const { setToken, getCookieOptions } = require("../utils/jwt");
 
 exports.login = (req, res) => {
-  setToken(res, req.body.firebaseToken);
+  setToken(res, req.body.firebaseToken, req);
   res.json({ message: "Login successful" });
 };
 
 exports.logout = (req, res) => {
-  res.clearCookie("token");
+  res.clearCookie("token", getCookieOptions(req));
   res.json({ message: "Logged out" });
 };
