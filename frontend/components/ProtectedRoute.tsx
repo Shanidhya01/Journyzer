1b2a2e6b6017456de4a5a476f3bb2c20b65ec350
Shanidhya01 <luckykumar0011s@gmail.com>
2026-01-19 13:30:21 +0530

    enhanced loading screen

--- frontend/components/ProtectedRoute.tsx
+++ frontend/components/ProtectedRoute.tsx
@@ -3,6 +3,7 @@
 import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import TripPlannerLoading from "./Loader";
 
 export default function ProtectedRoute({ children }: any) {
   const { user, loading } = useAuth();
@@ -12,7 +13,7 @@ export default function ProtectedRoute({ children }: any) {
     if (!loading && !user) router.replace("/login");
   }, [user, loading, router]);
 
-  if (loading) return <p className="p-10">Loading...</p>;
+  if (loading) return <p className="p-10"><TripPlannerLoading /></p>;
   if (!user) return null;
   return children;
 }
